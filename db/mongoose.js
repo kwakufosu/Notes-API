c9d894dd97372ca11d3ce47541431ed91fa09181
kwaku fosu <kwakuboachiefosu@gmail.com>
2022-01-05 00:23:00 +0000

    configured env

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://kwaks:<password>@cluster0.cxw0w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-mongoose.connect(MONGO_URL, { useNewUrlParser: true }, () => {
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log('connected to db');
 });
